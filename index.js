bf943d011ae6d0e4f2ffaad521f85587fe4f8f37
ruby0143 <mysoresriharsha07@gmail.com>
2023-01-20 22:39:31 +0530

    new route added (to get list of colleges)

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/college/:college",(req,res)=>{
   const college = req.params.college;
   console.log(college);
   // College.find({college_name : college},(err,docs)=>{
-  College.find({college_name : {'$regex' : `^${college}$`, '$options' : 'i'}},(err,docs)=>{
+  College.find({college_uuid : college},(err,docs)=>{
 
     if(!err){
       console.log(docs);
@@ -32,6 +32,19 @@ app.get("/college/:college",(req,res)=>{
     }
   })
 })
+
+app.get("/colleges",(req,res)=>{
+  College.find({},(err,docs)=>{
+    if(!err){
+      console.log(docs);
+      res.send(docs);
+    }
+    else{
+      res.send("404");
+    }
+  });
+});
+
 app.get("/",async (req,res)=>{
   res.send("OK");
   
@@ -39,47 +52,6 @@ app.get("/",async (req,res)=>{
 
 app.get("/add",(req,res)=>{
   res.send("ok");
-//   const college = new College({
-//     college_name: "IIT-BHU",
-//     header_photo_link :
-// "https://lh3.googleusercontent.com/p/AF1QipP9D06xtt7nFHnI7IQ8XyVUC_60L8…",
-// college_logo_link:
-// "https://www.iitbhu.ac.in/contents/iitbhu/img/other/iit_logo_original.p…",
-// overview:
-// "IIT BHU is one of the oldest engineering colleges in India, Founded in…",
-// nirf:
-// 29,
-// mode_of_admission:
-// "JOSAA",
-// exams:
-// "JEE Mains, JEE Advanced",
-// connectivity:
-// "RAILWAY abcd, AIRPORT abcd, METRO abcd, BUS abcd",
-// scholarships:
-// "1) 100% Tuition Fee waiver for SC/ST/PH students. 2) Full remission o…",
-// positives:
-// "Average package of about 13 LPU - strong Alumni base being one of the …",
-// negatives:
-// "Most of the hostels are decades old and are congested. Which are regre…",
-// highest_package:
-// 10,
-// average_package:
-// 10,
-// median_package:
-// 10,
-// top_recruiters:
-// "Amazon, Google, Microsoft",
-// review_video_link:
-// "https://www.youtube.com/watch?v=_gcgwGikYdk",
-//   });
-  // college.save((err,res)=>{
-  //   if(!err){
-  //     console.log("Successfully added");
-  //   }
-  //   else{
-  //     console.log(err);
-  //   }
-  // });
 })
 
 const PORT= process.env.PORT || 5000
